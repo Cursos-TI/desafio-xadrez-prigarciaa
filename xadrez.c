2007c433c23b9e2065f7e096e1f1b3cc924e9f32
Priscila Garcia <prigarcia620660@gmail.com>
2025-05-13 23:22:46 +0000

    criei o desafio xadrez nível novato.

--- xadrez.c
+++ xadrez.c
@@ -5,18 +5,43 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("### Desafio Xadrez ###\n");
+
+    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int casasTorre = 5;
+    int casasBispo = 4;
+    int casasRainha = 3;
+
+    // Nível Novato - Movimentação das Peças
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Mover a Torre 5 casa para direita
+    for (int i = 0; i < casasTorre; i++) 
+    {
 
-    // Implementação de Movimentação da Rainha
+        printf("Direita.\n"); //imprime a direção do movimento.
+    }
+    
+    // Implementação de Movimentação do Bispo.
+    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Movimentação do Bispo na diagonal direita.
+    for (int i = 0; i < casasBispo; i++)
+    {
+        printf("Diagonal Direita.\n"); //imprime a direção do movimento
+    }
+
+    // Implementação de Movimentação da Rainha.
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Movimentação da Rainha para a esquerda.
+    for (int i = 0; i < casasRainha; i++)
+    {
+        printf("Esquerda.\n"); //imprime a direção do movimento
+    }
 
+
+   
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
